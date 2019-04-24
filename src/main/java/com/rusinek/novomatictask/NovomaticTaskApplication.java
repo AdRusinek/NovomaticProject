63bd1875e40429ed957a461705dd9a577162b505
AdRusinek <rusinekadrian19@gmail.com>
2019-04-24 05:19:27 +0200

    first commit

---
+++ src/main/java/com/rusinek/novomatictask/NovomaticTaskApplication.java
@@ -0,0 +1,13 @@
+package com.rusinek.novomatictask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovomaticTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NovomaticTaskApplication.class, args);
+    }
+
+}
